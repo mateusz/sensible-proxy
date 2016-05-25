3ee40e805e5806130fe86cd4e39e2e4b0cfa0b52
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-05-26 09:48:13 +1200

    Refactor tests and add some error checking

--- sensible-proxy_test.go
+++ sensible-proxy_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -22,17 +23,9 @@ func (w *FakeWriter) Write(p []byte) (n int, err error) {
 func TestHTTPConnection(t *testing.T) {
 	w := &FakeWriter{}
 	appLog = log.New(w, "", log.Ldate|log.Ltime)
-	done := make(chan bool)
-	listener, err := getProxyServer(done, handleHTTPConnection)
-	conn, err := net.Dial("tcp", listener.Addr().String())
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	actual, err := requestDomainHTTP(conn, "google.com")
-	if err != nil {
-		t.Errorf("Error on connect: %s", err)
-	}
+	actual, conn, _ := requestHTTP("google.com")
+	defer conn.Close()
 
 	expected := "HTTP/1.0 302 Found"
 
@@ -48,40 +41,50 @@ func TestHTTPConnection(t *testing.T) {
 	if !strings.Contains(string(w.logs), expected) {
 		t.Errorf("Expected log to contain '%s' got:\n%s", expected, w.logs)
 	}
+}
 
+func TestHTTPConnectToNoneExistingDNS(t *testing.T) {
+	content, conn, _ := requestHTTP("t.ls")
 	defer conn.Close()
-	done <- true
+	if string(content) != "" {
+		t.Errorf("Expected read to be empty")
+	}
 }
 
 func TestHTTPSConnection(t *testing.T) {
-	w := &FakeWriter{}
-	appLog = log.New(w, "", log.Ldate|log.Ltime)
-	done := make(chan bool)
-	listener, err := getProxyServer(done, handleHTTPSConnection)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	conn, err := tls.Dial("tcp", listener.Addr().String(), &tls.Config{
-		ServerName: "google.com",
-	})
-	if err != nil {
-		t.Fatal("failed to connect: " + err.Error())
-	}
-
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\nHost: google.com\r\nContent-Length: 0\r\n\r\n")
-	actual, err := ioutil.ReadAll(conn)
+	actual, conn, err := requestHTTPS("google.com", "google.com")
+	defer conn.Close()
 	if err != nil {
 		t.Errorf("Error on read: %s", err)
+		return
 	}
-
 	expected := "HTTP/1.0 302 Found"
+	if !strings.Contains(string(actual), expected) {
+		t.Errorf("Expected response to contain '%s' got:\n%s", expected, actual)
+	}
+}
 
+func TestHTTPSConnectionEmptySNI(t *testing.T) {
+	_, _, err := requestHTTPS("", "google.com")
+	if err != io.EOF {
+		t.Errorf("Expected connection to be closed with an EOF")
+	}
+}
+
+func TestHTTPSConnectionWrongSNI(t *testing.T) {
+	w := &FakeWriter{}
+	appLog = log.New(w, "", log.Ldate|log.Ltime)
+	actual, conn, err := requestHTTPS("example.com", "google.com")
+	if err != nil {
+		t.Errorf("%s", err)
+		return
+	}
+	expected := "HTTP/1.0 404 Not Found"
 	if !strings.Contains(string(actual), expected) {
 		t.Errorf("Expected response to contain '%s' got:\n%s", expected, actual)
 	}
 
-	expected = "google.com"
+	expected = "example.com"
 	if !strings.Contains(string(w.logs), expected) {
 		t.Errorf("Expected log to contain '%s' got:\n%s", expected, w.logs)
 	}
@@ -89,14 +92,48 @@ func TestHTTPSConnection(t *testing.T) {
 	if !strings.Contains(string(w.logs), expected) {
 		t.Errorf("Expected log to contain '%s' got:\n%s", expected, w.logs)
 	}
-
-	defer conn.Close()
-	done <- true
 }
 
-func requestDomainHTTP(conn net.Conn, domain string) ([]byte, error) {
+func requestHTTP(domain string) ([]byte, net.Conn, error) {
+	done := make(chan bool)
+	defer func(doneChan chan bool) {
+		doneChan <- true
+	}(done)
+
+	server, err := getProxyServer(done, handleHTTPConnection)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer server.Close()
+
+	conn, err := net.Dial("tcp", server.Addr().String())
+	if err != nil {
+		return nil, nil, err
+	}
 	fmt.Fprintf(conn, "GET / HTTP/1.0\r\nHost: "+domain+"\r\nContent-Length: 0\r\n\r\n")
-	return ioutil.ReadAll(conn)
+	content, err := ioutil.ReadAll(conn)
+	return content, conn, err
+}
+
+func requestHTTPS(SNIServerName, requestServerName string) ([]byte, net.Conn, error) {
+	done := make(chan bool)
+	defer func(doneChan chan bool) {
+		doneChan <- true
+	}(done)
+	listener, err := getProxyServer(done, handleHTTPSConnection)
+	if err != nil {
+		return nil, nil, err
+	}
+	conn, err := tls.Dial("tcp", listener.Addr().String(), &tls.Config{
+		ServerName: SNIServerName,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\nHost: "+requestServerName+"\r\nContent-Length: 0\r\n\r\n")
+	content, err := ioutil.ReadAll(conn)
+	return content, conn, err
 }
 
 func getProxyServer(done chan bool, handler func(net.Conn)) (net.Listener, error) {
